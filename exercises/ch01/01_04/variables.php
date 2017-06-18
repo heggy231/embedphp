1f7a1a78ac0f249d6106446a45d714cdd88af4f6
Heggy Castaneda <heggyy@gmail.com>
2017-06-18 00:49:11 -0400

    update php dynamic content update title

--- exercises/ch01/01_04/variables.php
+++ exercises/ch01/01_04/variables.php
@@ -1,3 +1,11 @@
+<?php
+$answer = 42;
+$price = 10.99;
+// recommended practice for quote use single quote
+$title = "The Hitchhiker's Guide to the Galaxy";
+$author = 'Douglas Adams';
+$description = 'A hilarious romp. You will love the book, but maybe not the answer:';
+?>
 <!doctype html>
 <html>
 <head>
@@ -9,7 +17,7 @@
 <body>
 <h1>Editor's Choice</h1>
 <p>This week's recommended reading:</p>
-<h2>Book Title</h2>
+<h2><?php $title ?></h2>
 <p class="author">by A.N. Other</p>
 <p>Book description</p>
 <p>Price: </p>
